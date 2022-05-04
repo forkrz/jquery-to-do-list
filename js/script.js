c236353f997427b29f1e65fb59d6df68f1e8e119
forkrz <forkrz8@gmail.com>
2022-05-04 20:53:28 +0200

    vars changed to consts

--- js/script.js
+++ js/script.js
@@ -1,4 +1,60 @@
+function checkIfInputEmpty() {
+    return $("#Text1").val();
+}
+
+function checkIfConfirmationBtnClicked() {
+    return $("#exampleCheck1").is(':checked');
+}
+
+function addNotesFromJson() {
+    $.get("https://jsonplaceholder.typicode.com/users", function (data) {
+        const arr = data;
+        $.each(arr,function (index){
+            addNoteFromJson(index +1,arr[index].company.catchPhrase);
+            addListener(index + 1);
+        })
+    })
+}
+
+function addListener(index){
+    $("[delete-id=" + index + "]").click(function (){
+        $(this).closest('tr').remove();
+    })
+}
+
+function addNoteFromJson(id,text){
+    const row = $("<tr></tr>");
+    const idCell = $("<td id=row"+id + ">" + id + "</td>");
+    const textCell = $("<td>" + text + "</td>");
+    const updateBtn = $("<button type=\"button\" class=\"btn btn-warning\">Update</button>");
+    const deleteBtn = $("<button type=\"button\" class=\"btn btn-danger\">Delete</button>");
+    updateBtn.attr('update-id',id);
+    deleteBtn.attr('delete-id',id);
+    const tdBtn1 = $("<td></td>")
+    const tdBtn2 = $("<td></td>")
+    tdBtn1.append(updateBtn);
+    tdBtn2.append(deleteBtn);
+    $("#tbody").append(row);
+    $("tr:last").append(idCell,textCell,tdBtn1,tdBtn2);
+}
+
+     function addNote(){
+    const row = $("<tr><th scope='row'></th></tr>");
+    if(!checkIfInputEmpty()){
+        alert('insert some text!')
+        return;
+    }
+    if(!checkIfConfirmationBtnClicked()){
+        alert('click confirmation button.')
+        return;
+    }
+
+    $("#tbody").append(row);
+    $("th:last").append(id);
+}
+
 $(document).ready(function() {
+
     const table = $("<table class='table'></table>");
     const thead = $("<thead class='table-dark''></thead>")
     const headers = $("<tr><th>ID</th><th>TEXT</th><th>UPDATE</th><th>DELETE</th></tr>");
@@ -17,11 +73,8 @@ $(document).ready(function() {
         "</form>");
 
 
-    $("#wrapper").append(addButton);
-    $("#wrapper").append(addNoteForm);
-    $("#wrapper").append(table);
-    $(".table").append(thead);
-    $(".table").append(tbody);
+    $("#wrapper").append(addButton,addNoteForm,table);
+    $(".table").append(thead,tbody);
     $(".table-dark").append(headers);
 
 
@@ -29,65 +82,11 @@ $(document).ready(function() {
         $("#addNoteForm").toggle();
     });
 
-    function checkIfInputEmpty() {
-        return $("#Text1").val();
-    }
-
-    function checkIfConfirmationBtnClicked() {
-        return $("#exampleCheck1").is(':checked');
-    }
-
-    function addNotesFromJson() {
-        $.get("https://jsonplaceholder.typicode.com/users", function (data) {
-            var arr = data;
-            $.each(arr,function (index){
-                addNoteFromJson(index +1,arr[index].company.catchPhrase);
-                addListener(index + 1);
-            })
-        })
-    }
-
-    function addListener(index){
-        $("[delete-id=" + index + "]").click(function (){
-            $(this).closest('tr').remove();
-        })
-    }
-
-    function addNoteFromJson(id,text){
-        var row = $("<tr></tr>");
-        var idCell = $("<td id=row"+id + ">" + id + "</td>");
-        var textCell = $("<td>" + text + "</td>");
-        var updateBtn = $("<button type=\"button\" class=\"btn btn-warning\">Update</button>");
-        var deleteBtn = $("<button type=\"button\" class=\"btn btn-danger\">Delete</button>");
-        updateBtn.attr('update-id',id);
-        deleteBtn.attr('delete-id',id);
-        var tdBtn1 = $("<td></td>")
-        var tdBtn2 = $("<td></td>")
-        tdBtn1.append(updateBtn);
-        tdBtn2.append(deleteBtn);
-        $("#tbody").append(row);
-        $("tr:last").append(idCell,textCell,tdBtn1,tdBtn2);
-    }
-
-        addNotesFromJson();
-
-    var addNote = function (){
-        var row = $("<tr><th scope='row'></th></tr>");
-        if(!checkIfInputEmpty()){
-            alert('insert some text!')
-            return;
-        }
-        if(!checkIfConfirmationBtnClicked()){
-            alert('click confirmation button.')
-            return;
-        }
 
 
-        $("#tbody").append(row);
-        $("th:last").append(id);
-    }
+    $("#addNoteFormBtn").click(()=>{addNote()});
 
-    $("#addNoteFormBtn").click(addNote);
+    addNotesFromJson();
 
 
 
